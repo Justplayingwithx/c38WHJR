bb6cf19e3d7209f578d9205136786fbac0d48429
Justplayingwithx <75589084+Justplayingwithx@users.noreply.github.com>
2021-02-28 19:10:36 -0500

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,85 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+
+
+    }
+
+      car1 = createSprite(50,130)
+      car2 = createSprite(70,130)
+      car3 = createSprite(90,130)
+      car4 = createSprite(110,130)
+
+      cars = [car1,car2,car3,car4]
+
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      //var display_position = 130;
+      var loopIndex = 0;
+      var x = 0;
+      var y = 0;
+      
+      for(var plr in allPlayers){
+        loopIndex++ 
+        x = x + 200;
+        y = displayHeight-allPlayers[plr].distance
+        cars[loopIndex-1].x = x;
+        cars[loopIndex-1].y = y;
+
+        if(loopIndex === player.index){
+          cars[loopIndex-1].shapeColor = "blue"
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[loopIndex-1].y
+        }
+
+       /* if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)*/
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+    console.log("hi")
+    drawSprites();
+    console.log("xxx")
+  }
+}
